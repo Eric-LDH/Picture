3f86e6891c7dac3ce552cfe15d9fda497f040008
Eric-LDH <387223669@qq.com>
2019-09-29 17:07:26 +0800

    增加首页按钮
    Signed-off-by: Eric-LDH <387223669@qq.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <van-divider
-      content-position="left"
-      :style="{ color: '#1989fa', borderColor: '#1989fa', padding: '0 16px' }"
-    >9月24日 星期二</van-divider>
-    <van-grid :column-num="3" :border="false" :gutter="10">
-      <van-grid-item v-for="value in 6" :key="value">
-        <van-image v-bind:src="'https://img.yzcdn.cn/vant/apple-'+value+'.jpg'" />
+    <van-grid clickable :border="false" :square="true" :column-num="4">
+      <van-grid-item icon="add" text="上传" :style="{color:'#66CD00'}" />
+      <van-grid-item icon="star" text="收藏" :style="{color:'#CDCD00'}" />
+      <van-grid-item icon="warning" text="清理" />
+    </van-grid>
+    <van-sticky>
+      <van-cell title="9月24日 星期二" :title-style="{ color: '#1989fa',textAlign: 'left' }" />
+    </van-sticky>
+    <van-grid :column-num="3" :center="true" :border="false" square>
+      <van-grid-item v-for="value in 10" :key="value">
+        <van-image
+          fit="cover"
+          height="100%"
+          :src="'https://img.yzcdn.cn/vant/apple-'+value+'.jpg'"
+        />
       </van-grid-item>
     </van-grid>
   </div>
