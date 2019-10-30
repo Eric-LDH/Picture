da4754b787acf85610f0e52604daad47b298b033
Eric-LDH <387223669@qq.com>
2019-10-30 09:41:10 +0800

    修改上传界面

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,28 +1,58 @@
 <template>
   <div>
-    <van-nav-bar title="上传" left-text="返回" left-arrow @click-left="goHome" />
-
-    <van-cell-group :border="false">
+    <van-nav-bar title="上传" left-text="返回" left-arrow fixed @click-left="goHome" />
+    <van-cell-group :border="false" :style="{marginTop:'50px',marginBottom:'50px'}">
       <van-cell>
-        <van-uploader :after-read="afterRead" v-model="imgList" multiple />
-      </van-cell>
-      <van-cell>
-        <van-button color="linear-gradient(to right, #4bb0ff, #6149f6)" size="large">提交</van-button>
+        <van-uploader :before-read="beforeRead" :after-read="afterRead" v-model="imgList" multiple />
       </van-cell>
     </van-cell-group>
+    <van-submit-bar
+      :price="capacityTotal()"
+      :disabled="imgList.length == 0"
+      button-text="提交"
+      currency="∶"
+      suffix-label="MB"
+      button-type="info"
+      label="合计"
+      :loading="loading"
+      @submit="sumbit"
+    />
+    <van-skeleton title :row="3" :loading="loading" />
   </div>
 </template>
 <script>
 export default {
   data() {
-    return { imgList: [] };
+    return { imgList: [], loading: false };
   },
   methods: {
     goHome() {
       this.$router.push("/");
     },
+    beforeRead() {
+      this.loading = true;
+
+      return true;
+    },
     afterRead(file) {
       console.log(file);
+      this.loading = false;
+    },
+    capacityTotal() {
+      if (this.imgList.length == 0) {
+        return 0;
+      } else {
+        let total = 0;
+        for (let i = 0; i < this.imgList.length; i++) {
+          total += this.imgList[i].file.size;
+        }
+
+        return (total / 1024 / 1024) * 100;
+      }
+    },
+    sumbit() {
+      this.loading = true;
+      console.log(this.imgList);
     }
   }
 };
