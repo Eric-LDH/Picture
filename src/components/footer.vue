9f7532534a79df876c957dbf75835707a27e4c10
Eric-LDH <387223669@qq.com>
2019-11-08 11:49:16 +0800

    修改故事栏目

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <van-tabbar v-bind:route="true">
+    <van-tabbar :route="true" style="box-shadow:0px 0px 3px #333333">
       <van-tabbar-item to="/" icon="home-o">照片</van-tabbar-item>
       <van-tabbar-item to="/story" icon="smile-o">故事</van-tabbar-item>
       <van-tabbar-item icon="friends-o">我</van-tabbar-item>
