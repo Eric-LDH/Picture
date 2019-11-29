9331da076a65c6b7abd624a5992453c1a7f36c95
Eric-LDH <387223669@qq.com>
2019-11-29 15:35:46 +0800

    登录页样式修改
    Signed-off-by: Eric-LDH <387223669@qq.com>

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="bg">        
-    <!-- <van-image height="100%" src="../assets/loginbg.jpg" /> -->
-    </div>
+  <div class="bg"></div>
 </template>
 <style scoped>
-    .bg{
-        height:100%;
-        background-image: url("../assets/loginbg.jpg");
-        background-position: center center;
-        background-size: cover;
-    }
+.bg {
+  height: 100%;
+  background-image: url("../assets/loginbg.jpg");
+  background-position: center center;
+  background-size: cover;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
 </style>
